cf3828069fc97e7a63fabc2ac858d0d002cc6757
kudo2382k4 <kudokdh2308@gmail.com>
2026-01-18 00:11:02 +0700

    create project

---
+++ src/main/java/com/fpt/edu/lifeform/LifeformApplication.java
@@ -0,0 +1,13 @@
+package com.fpt.edu.lifeform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifeformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LifeformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fpt/edu/lifeform/LifeformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fpt.edu.lifeform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifeformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
